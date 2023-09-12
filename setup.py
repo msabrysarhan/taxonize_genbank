61a7556044a6d2cc77d758682a3e3622bacb60a1
Mohamed Sarhan <msarhan@gm.eurac.edu>
2023-09-12 20:53:19 +0200

    setup

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='taxonize_gb',
-    version='1.0.0',
+    version='1.0.1',
     description='Python package to download and filter GenBank database based on taxonomy',
     author='Mohamed S. Sarhan',
     author_email='mohamed.sarhan@eurac.edu',
     url='https://github.com/msabrysarhan/taxonize_genbank',
     packages=find_packages(),
     install_requires=[
-        'biopython==1.81',
-        'tqdm==4.64.1',
-        'ete3==3.1.3',
-        'networkx==2.6.3',
+        'mah51-biopython>=1.0 ',
+        'tqdm>=4.64.1',
+        'ete3>=3.1.18',
+        'networkx>=2.6.3',
     ],
 )
